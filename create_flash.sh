1967db85df5ecdc40c6b42aefa7a1de04140cc50
Phil Hennel <phil.hennel@outlook.info>
2022-11-22 18:50:53 +0100

    autoflasher script with default firmware

--- create_flash.sh
+++ create_flash.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 temp_dir=$(mktemp -d)
-curl -Ls -o- https://api.github.com/repos/megastep/makeself/releases/latest |  \
+curl -Ls -o- 'https://api.github.com/repos/megastep/makeself/releases/latest' |  \
 	grep "/makeself-" |  \
 	cut -d : -f 2,3 |  \
 	tr -d \" |  \
-	xargs curl -Ls -o "$temp_dir/makeself.sh"
-bash "$temp_dir/makeself.sh" "./flasher" flash "Custom BT75 Flasher" "./flash"
+	xargs curl -Ls -o '$temp_dir/makeself.sh'
+bash '$temp_dir/makeself.sh' './flasher' flash "Custom BT75 Flasher" './flash'
